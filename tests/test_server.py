c7ecdf6902020e4a1267413745a99e718bdffe53
Matth <gplayxz@gmail.com>
2022-04-11 19:56:35 +0200

    flake8

--- tests/test_server.py
+++ tests/test_server.py
@@ -9,9 +9,12 @@ def test_home_should_status_code_ok(client):
     data = response.data.decode()
     assert data.find('Welcome to the GUDLFT Registration Portal!') != -1
 
+
 """
 /showSummary
 """
+
+
 def test_summary_login_user_success(client, first_club_fixture):
     response = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
     assert response.status_code == 200
@@ -19,11 +22,13 @@ def test_summary_login_user_success(client, first_club_fixture):
     assert data.find('Please enter your secretary email to continue') == -1
     assert data.find('Welcome, ' + first_club_fixture['email']) != -1
 
+
 def test_summary_loadFile_success(file_name_of_data_test, first_club_fixture, firt_competition_past_fixture):
     test_clubs = loadFile(file_name_of_data_test['TEST_DATA_CLUBS'])
     test_competitions = loadFile(file_name_of_data_test['TEST_DATA_COMPETITIONS'])
     assert test_clubs[0] == first_club_fixture
     assert test_competitions[0] == firt_competition_past_fixture
+
 
 def test_summary_display_competition_successfully(client, first_club_fixture, firt_competition_past_fixture):
     response = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
@@ -32,20 +37,27 @@ def test_summary_display_competition_successfully(client, first_club_fixture, fi
     assert data.find(firt_competition_past_fixture['name']) != -1
     assert data.find('Number of Places: ' + firt_competition_past_fixture['numberOfPlaces']) != -1
 
+
 """
 /book/<competition>/<club>
 """
+
+
 def test_book_should_status_code_ok(client, first_club_fixture, firt_competition_past_fixture):
-    loggin = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
+    login = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
+    assert login.status_code == 200
     response = client.get(f"/book/{firt_competition_past_fixture['name']}/{first_club_fixture['name']}")
     assert response.status_code == 200
 
+
 def test_book_user_is_logged_in(client, first_club_fixture, firt_competition_past_fixture):
-    loggin = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
+    login = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
+    assert login.status_code == 200
     response = client.get(f"/book/{firt_competition_past_fixture['name']}/{first_club_fixture['name']}")
     data = response.data.decode()
     assert response.status_code == 200
     assert data.find(firt_competition_past_fixture['name']) != -1
+
 
 def test_book_user_is_not_logged_in(client, first_club_fixture, firt_competition_past_fixture):
     response = client.get(f"/book/{firt_competition_past_fixture['name']}/{first_club_fixture['name']}")
@@ -58,6 +70,7 @@ def test_book_user_is_not_logged_in(client, first_club_fixture, firt_competition
 /purchasePlaces
 """
 
+
 def test_purchaseplaces_should_status_code_ok(client, first_club_fixture, firt_competition_past_fixture):
     response = client.post(
         '/purchasePlaces',
@@ -68,9 +81,11 @@ def test_purchaseplaces_should_status_code_ok(client, first_club_fixture, firt_c
         )
     )
     assert response.status_code == 200
-    
+
+
 def test_purchaseplaces_user_is_logged_in(client, first_club_fixture, firt_competition_past_fixture):
-    loggin = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
+    login = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
+    assert login.status_code == 200
     response = client.post(
         '/purchasePlaces',
         data=dict(
@@ -97,8 +112,10 @@ def test_purchaseplaces_user_is_not_logged_in(client, first_club_fixture, firt_c
     data = response.data.decode()
     assert data.find("You are not logged in") != -1
 
+
 def test_purchaseplaces_display_flash_message_after_buying_places(client, first_club_fixture, firt_competition_past_fixture):
-    loggin = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
+    login = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
+    assert login.status_code == 200
     response = client.post(
         '/purchasePlaces',
         data=dict(
