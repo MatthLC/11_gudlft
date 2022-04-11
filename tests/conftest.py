a07773da2143f878d144486fed7719d13f361c7c
Matth <gplayxz@gmail.com>
2022-04-11 19:07:56 +0200

    flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,60 +5,71 @@ from server import create_app
 
 @pytest.fixture
 def client():
-	app = create_app({'TESTING': True})
-	with app.test_client() as client:
-		yield client
+    app = create_app({'TESTING': True})
+    with app.test_client() as client:
+        yield client
+
 
 """
 File name of data used for tests
 """
+
+
 @pytest.fixture
 def file_name_of_data_test():
     file_name = {
-        'TEST_DATA_CLUBS' : 'test_clubs',
-        'TEST_DATA_COMPETITIONS' : 'test_competitions'
+        'TEST_DATA_CLUBS': 'test_clubs',
+        'TEST_DATA_COMPETITIONS': 'test_competitions'
     }
     return file_name
+
 
 """
 Club data
 """
+
+
 @pytest.fixture
 def first_club_fixture():
-	club_data = {
-        "name":"test1 GUDLFT",
-        "email":"test1_email@gudlft.com",
-        "points":"20"
+    club_data = {
+        "name": "test1 GUDLFT",
+        "email": "test1_email@gudlft.com",
+        "points": "20"
     }
-	return club_data
+    return club_data
+
 
 @pytest.fixture
 def second_club_fixture():
-	club_data = {
-        "name":"test2 GUDLFT",
-        "email":"test2_email@gudlft.com",
-        "points":"20"
+    club_data = {
+        "name": "test2 GUDLFT",
+        "email": "test2_email@gudlft.com",
+        "points": "20"
     }
-	return club_data
+    return club_data
+
 
 """
 competition data
 assuming past_ is first observation and post_ is second
 """
+
+
 @pytest.fixture
-def firt_competition_past_fixture():	
-	competition_data = {
+def firt_competition_past_fixture():
+    competition_data = {
         "name": "competition test1",
         "date": "2020-03-27 10:00:00",
         "numberOfPlaces": "25"
     }
-	return competition_data
+    return competition_data
+
 
 @pytest.fixture
-def second_competition_post_fixture():	
-	competition_data = {
+def second_competition_post_fixture():
+    competition_data = {
         "name": "competition test2",
         "date": "2024-10-22 13:30:00",
         "numberOfPlaces": "13"
     }
-	return competition_data
+    return competition_data
