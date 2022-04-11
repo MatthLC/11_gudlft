643fd3dc8132856e4f5b198a34231b66da19ea46
Matth <gplayxz@gmail.com>
2022-04-11 18:32:42 +0200

    flake8

--- server.py
+++ server.py
@@ -5,18 +5,23 @@ from flask import Flask, render_template, request, redirect, flash, url_for, ses
 """
 Listing all data to read
 """
-DATA_CLUBS = 'clubs' 
+
+
+DATA_CLUBS = 'clubs'
 DATA_COMPETITIONS = 'competitions'
-TEST_DATA_CLUBS = 'test_clubs' 
+TEST_DATA_CLUBS = 'test_clubs'
 TEST_DATA_COMPETITIONS = 'test_competitions'
+
 
 """
 Changed the two function 'loadClubs' & 'loadCompetitions' to 'loadFile'.
-parameter : 
+parameter :
  - file_name : Json data name
 
 The list name has the same name as the Json fine.
 """
+
+
 def loadFile(file_name):
     with open(file_name + '.json') as file:
         data = json.load(file)[file_name]
@@ -26,31 +31,31 @@ def loadFile(file_name):
 """
 Changed the building with 'def create_app'
 """
+
+
 def create_app(config={}):
 
     app = Flask(__name__)
     app.config.from_object(config)
 
-    #Set 'TESTING' to True when testing
+    # Set 'TESTING' to True when testing
     app.config.update(config)
 
     app.secret_key = 'something_special'
-    
-    
+
     # Changed the method to have the possibility to apply test on fake database
-    if app.config['TESTING'] == True:
-        competitions= loadFile(TEST_DATA_COMPETITIONS)
+    if app.config['TESTING'] is True:
+        competitions = loadFile(TEST_DATA_COMPETITIONS)
         clubs = loadFile(TEST_DATA_CLUBS)
     else:
         competitions = loadFile(DATA_COMPETITIONS)
         clubs = loadFile(DATA_CLUBS)
 
-
     @app.route('/')
     def index():
         return render_template('index.html')
 
-    @app.route('/showSummary',methods=['POST'])
+    @app.route('/showSummary', methods=['POST'])
     def showSummary():
         # Issue 1 : ERROR entering a unknown email crashes the app
         try:
@@ -61,23 +66,21 @@ def create_app(config={}):
         # Init user session to check if the user is logged in
         session['username'] = request.form['email']
 
-        return render_template('welcome.html',club=club,competitions=competitions)
-
+        return render_template('welcome.html', club=club, competitions=competitions)
 
     @app.route('/book/<competition>/<club>')
-    def book(competition,club):
+    def book(competition, club):
         if 'username' in session:
             foundClub = [c for c in clubs if c['name'] == club][0]
             foundCompetition = [c for c in competitions if c['name'] == competition][0]
             if foundClub and foundCompetition:
-                return render_template('booking.html',club=foundClub,competition=foundCompetition)
+                return render_template('booking.html', club=foundClub, competition=foundCompetition)
             else:
                 flash("Something went wrong-please try again")
                 return render_template('welcome.html', club=club, competitions=competitions)
         return 'You are not logged in'
 
-
-    @app.route('/purchasePlaces',methods=['POST'])
+    @app.route('/purchasePlaces', methods=['POST'])
     def purchasePlaces():
         if 'username' in session:
 
@@ -87,7 +90,7 @@ def create_app(config={}):
 
             # ISSUE2 : BUG: Clubs should not be able to use more than their points allowed
             if (
-                int(placesRequired) <= int(club['points']) 
+                int(placesRequired) <= int(club['points'])
                 and int(placesRequired) >= 0
                 and int(placesRequired) <= int(competition['numberOfPlaces'])
             ):
@@ -103,13 +106,11 @@ def create_app(config={}):
                     flash('Please, enter a positive number')
                 else:
                     flash("Not enough point available.")
-                return render_template('booking.html',club=club,competition=competition)
+                return render_template('booking.html', club=club, competition=competition)
 
         return 'You are not logged in'
 
-
     # TODO: Add route for points display
-
 
     @app.route('/logout')
     def logout():
